6d26a42eb1eaa38882e04efe7602daec11422086
Thomas Cilloni <thomas.martin.cilloni@gmail.com>
2019-04-30 00:41:37 +0800

    Fixed bugs and removed unnecessary methods

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -4,13 +4,10 @@ public class Scheduler {
     private int[][] Z = {
             {3, 5},
             {4, 7},
-            {3, 3},
+            {2, 3},
             {3, 4}
     };
-    private boolean added[] = new boolean[Z.length];
-    private int bestItem[] = new int[Z[0].length];
-    private int count = 0;
-    private int totalCost = 0;
+    private boolean[] added = new boolean[Z.length];
     private LinkedList<Integer>[] bestItems;
 
     public static void main(String[] args) {
@@ -18,38 +15,42 @@ public class Scheduler {
     }
 
     private Scheduler() {
+        // initialize best items
+        bestItems = (LinkedList<Integer>[]) new LinkedList[Z[0].length];
         generateQueues();
 
         // at the beginning no items are added
         for (int i=0; i<added.length; i++)
             added[i] = false;
 
-        // initialize the first best items per factory
-        for (int i=0; i<Z[0].length; i++)
-            updateBestItem(i);
-
+        //private int bestItem[] = new int[Z[0].length];
+        int count = 0;
+        int totalCost = 0;
         while (count < Z.length) {
+            // review which one is the best item to produce for each factory
+            updateBestItems();
+
             // get the best factory to produce the next item
             int bestFactory = getBestFactory();
+            int item = bestItems[bestFactory].get(0);
 
             // produce the item. to produce it, do:
             //  1. change the 'added' flag
             //  2. add its cost to the total cost
             //  3. update the cost of other items in the same factory
-            System.out.printf("Picked item %d with cost %d\n", bestItem[bestFactory], Z[bestItem[bestFactory]][bestFactory]);
-            added[bestItem[bestFactory]] = true;
-            totalCost += Z[bestItem[bestFactory]][bestFactory];
-            updateCosts(bestFactory, Z[bestItem[bestFactory]][bestFactory]);
-
-            // must review which one is the best item to produce now
-            updateBestItem(bestFactory);
+            added[item] = true;
+            totalCost += Z[item][bestFactory];
+            updateCosts(bestFactory, Z[item][bestFactory]);
 
             // one more item added
             count++;
+
+            // log
+            System.out.printf("Picked item %d with cost %d\n", item, Z[item][bestFactory]);
         }
 
         System.out.println("Best tot cost: " + totalCost);
-        System.out.println("Best avg cost: " + ((double) totalCost)/count);
+        System.out.println("Best avg cost: " + ((double) totalCost)/ count);
     }
 
     private void generateQueues() {
@@ -60,38 +61,37 @@ public class Scheduler {
             LinkedList<Integer> list = new LinkedList<>();
             list.add(0);
 
-            for (int i=1; i<Z.length; i++)
-                for (int pos=0; i<list.size(); pos++)
-                    if (Z[list.get(pos)][f] > Z[i][f])
-                        list.add(pos, i);
+            for (int i=1; i<Z.length; i++) {
+                int pos = 0;
+                while (pos<list.size()) {
+                    if (Z[list.get(pos)][f] < Z[i][f])
+                        pos++;
+                    else
+                        break;
+                }
+                list.add(pos, i);
+            }
 
             // and put the list in the array of lists
             bestItems[f] = list;
         }
     }
 
-    private void updateBestItem(int factory) {
-        int leastCost = 0;
-        while (added[leastCost])
-            leastCost++;
-
-        for (int item=1; item<Z.length; item++)
-            if (!added[item])
-                if (Z[item][factory] < Z[leastCost][factory])
-                    leastCost = item;
-
-        bestItem[factory] = leastCost;
+    private void updateBestItems() {
+        for (LinkedList<Integer> itemsQueue : bestItems)
+            while (added[itemsQueue.get(0)])
+                itemsQueue.remove(0);
     }
 
     private int getBestFactory() {
-        int leastCost = Z[bestItem[0]][0];
+        int leastCost = Integer.MAX_VALUE;
         int indexLeastCost = 0;
 
         // check every factory to pick the factory who
         // can make the next item the fastest
-        for (int i=0; i<bestItem.length; i++) {
-            if (Z[bestItem[i]][i] < leastCost) {
-                leastCost = Z[bestItem[i]][i];
+        for (int i=0; i<bestItems.length; i++) {
+            if (Z[bestItems[i].get(0)][i] < leastCost) {
+                leastCost = Z[bestItems[i].get(0)][i];
                 indexLeastCost = i;
             }
         }
