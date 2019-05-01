e60c02dd9965f23107117185176466a9b581fd99
Thomas Cilloni <thomas.martin.cilloni@gmail.com>
2019-05-02 00:31:04 +0800

    Added file reader

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,34 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Scheduler {
-    private int[][] Z = {
-            {1, 5, 4, 1, 3},
-            {4, 7, 20, 2, 4},
-            {2, 3, 9, 3, 5},
-            {3, 4, 15, 4, 2}
-    };
 //    private int[][] Z = {
+//            {1, 5, 4, 1, 3},
+//            {4, 7, 20, 2, 4},
+//            {2, 3, 9, 3, 5},
+//            {3, 4, 15, 4, 2}
+//    };
+    private int[][] Z;
+    //    private int[][] Z = {
 //            {1, 100, 100},
 //            {99, 1, 99},
 //            {98, 1, 98}
 //    };
-    private boolean[] added = new boolean[Z.length];
+    private boolean[] added;
     private LinkedList<Integer>[] bestItems;
 
     // waiting time for each factory
-    private int[] W = new int[Z[0].length];
+    private int[] W;
 
     public static void main(String[] args) {
         new Scheduler();
     }
 
     private Scheduler() {
+        String filename = "data.txt";
+        try {
+            // instantiate the file reader
+            IO io = new IO(filename);
+
+            // read the number of tests
+            int tests = io.getTestCases();
+
+            for (int test = 0; test < tests; test++) {
+                System.out.printf("Test %d has %d items in %d factories\n",
+                        test, io.getItemNumber(test), io.getFactoryNumber(test));
+            }
+
+            Z = io.getTestMatrix(1);
+            W = new int[Z[0].length];
+            added = new boolean[Z.length];
+
+        } catch (IOException e) {
+            System.out.printf("Error loading file. Make sure it is '%s'", filename);
+            return;
+        }
+
         // initialize best items
         bestItems = (LinkedList<Integer>[]) new LinkedList[Z[0].length];
         generateQueues();
 
         // at the beginning no items are added
-        for (int i=0; i<added.length; i++)
+        for (int i = 0; i < added.length; i++)
             added[i] = false;
 
         int totalCost = 0;
@@ -53,20 +80,20 @@ public class Scheduler {
         }
 
         System.out.println("Best tot cost: " + totalCost);
-        System.out.printf("Best avg cost: %.6f", ((double) totalCost)/ Z.length);
+        System.out.printf("Best avg cost: %.6f", ((double) totalCost) / Z.length);
     }
 
     private void generateQueues() {
         // for every factory
-        for (int f=0; f<Z[0].length; f++) {
+        for (int f = 0; f < Z[0].length; f++) {
 
             // make a sorted list of item indices
             LinkedList<Integer> list = new LinkedList<>();
             list.add(0);
 
-            for (int i=1; i<Z.length; i++) {
+            for (int i = 1; i < Z.length; i++) {
                 int pos = 0;
-                while (pos<list.size()) {
+                while (pos < list.size()) {
                     if (Z[list.get(pos)][f] < Z[i][f])
                         pos++;
                     else
@@ -92,7 +119,7 @@ public class Scheduler {
 
         // check every factory to pick the factory who
         // can make the next item the fastest
-        for (int f=0; f<bestItems.length; f++) {
+        for (int f = 0; f < bestItems.length; f++) {
             if (Z[bestItems[f].get(0)][f] + W[f] < leastCost) {
                 leastCost = Z[bestItems[f].get(0)][f] + W[f];
                 indexLeastCost = f;
@@ -100,5 +127,54 @@ public class Scheduler {
         }
 
         return indexLeastCost;
+    }
+
+
+    private class IO {
+        private List<String> lines;
+        private List<Integer> testHead = new ArrayList<>();
+
+        IO (String filename) throws IOException {
+            lines = Files.readAllLines(Paths.get(filename));
+
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).isBlank())
+                    testHead.add(i+1);
+            }
+        }
+
+        int getTestCases() {
+            return Integer.parseInt(lines.get(0));
+        }
+
+        int getItemNumber(int testCase) {
+            return Integer.parseInt(
+                    lines.get(testHead.get(testCase)) // line containing "3 4"
+                            .split(" ")[0] // get the first number
+            );
+        }
+
+        int getFactoryNumber(int testCase) {
+            return Integer.parseInt(
+                    lines.get(testHead.get(testCase)) // line containing "3 4"
+                            .split(" ")[1] // get the second number
+            );
+        }
+
+        int[][] getTestMatrix(int testCase) {
+            int items = getItemNumber(testCase);
+            int factories = getFactoryNumber(testCase);
+            int[][] Z = new int[items][factories];
+
+            for (int i = 0; i < items; i++) {
+                String[] costs = lines.get(testHead.get(testCase)+1 + i).split(" ");
+
+                for (int c = 0; c < costs.length; c++) {
+                    Z[i][c] = Integer.parseInt(costs[c]);
+                }
+            }
+
+            return Z;
+        }
     }
 }
